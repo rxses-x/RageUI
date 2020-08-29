7b061dcb462acda3b494e4ae403b4befba56f0fe
Dylan Malandain <lucenzo.76@gmail.com>
2020-08-29 05:12:38 +0200

    Rebase RageUI components

--- components/Sprite.lua
+++ components/Sprite.lua
@@ -1,18 +1,20 @@
+---RenderSprite
 ---
---- @author Dylan MALANDAIN
---- @version 2.0.0
---- @since 2020
+--- Reference : https://github.com/iTexZoz/NativeUILua_Reloaded/blob/master/UIElements/Sprite.lua#L90
 ---
---- RageUI Is Advanced UI Libs in LUA for make beautiful interface like RockStar GAME.
----
----
---- Commercial Info.
---- Any use for commercial purposes is strictly prohibited and will be punished.
----
---- @see RageUI
----
-
-
+---@param TextureDictionary string
+---@param TextureName string
+---@param X number
+---@param Y number
+---@param Width number
+---@param Height number
+---@param Heading number
+---@param R number
+---@param G number
+---@param B number
+---@param A number
+---@return nil
+---@public
 function RenderSprite(TextureDictionary, TextureName, X, Y, Width, Height, Heading, R, G, B, A)
     ---@type number
     local X, Y, Width, Height = (tonumber(X) or 0) / 1920, (tonumber(Y) or 0) / 1080, (tonumber(Width) or 0) / 1920, (tonumber(Height) or 0) / 1080
